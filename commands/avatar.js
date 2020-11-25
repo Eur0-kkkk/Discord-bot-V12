04d912750abf60ef8e0630aeb39c9148bf7f1508
K1rito <70243197+Eur0-kkkk@users.noreply.github.com>
2020-11-25 08:14:43 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,5 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-exports.run = async (client, message,args) => {
-
+exports.run = (client, message, args) => {
+      let user = message.mentions.users.first() || message.author
+      const embed = new Discord.MessageEmbed()
+            .setTitle(`🖼️ ${user.tag}`)
+            .setDescription(`**Clique [aqui](${user.displayAvatarURL({ size: 1024, dynamic: true })}) para baixar a imagem!**`)
+            .setImage(user.displayAvatarURL({ size: 1024, dynamic: true }))
+            .setColor('#8A2BE2')
+            .setFooter(
+				`Requisitado: ${message.author.username}`,
+				message.author.displayAvatarURL({ dynamic: true })
+			);
+    message.channel.send(embed)
 }
