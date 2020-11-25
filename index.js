160a634d07d02ea8d72d6091ddcf4fd52b31ad30
K1rito <70243197+Eur0-kkkk@users.noreply.github.com>
2020-11-25 08:13:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const moment = require("moment");
+const config = require("./config.json");
+const client = new Discord.Client();
+const db = require("quick.db");
+const msa = "ping?";
+const fs = require("fs");
+client.fs = fs;
+const request = require("request");
+const { CanvasSenpai } = require("canvas-senpai");
+const canva = new CanvasSenpai();
+
+client.on("message", async message => {
+  if (!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content
+    .trim()
+    .slice(config.prefix.length)
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  try {
+    const commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+    message.delete();
+  }
+});
+
+client.on("ready", () => {
+  console.log(
+    `Iniando..`
+  );
+});
